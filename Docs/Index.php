d975077d70672e6df3b3d12ae5b3288a0a17d590
wanderson22 <wandersondrs@hotmail.com>
2016-03-22 22:16:33 -0300

    Rename Index.php.txt to Index.php

---
+++ Docs/Index.php
@@ -0,0 +1,12 @@
+Ao usar o brower eu busco a infromação (uso)
+
+private pois garante que os atributos não podem ser alterados.
+public faz com que vc tenha acesso aos atributos
+
+Só pode executar uma ação através de uma função.
+
+Para manipular o modelo eu crio um controle de gerenciamento.
+
+JSON_ENCODE é uma formatação.
+
+var_dump
