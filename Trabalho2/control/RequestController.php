bbbc9f1630e4608a052bea72a3b51eec080a4183
wanderson22 <wandersondrs@hotmail.com>
2016-04-17 21:36:00 -0300

    Update RequestController.php

--- Trabalho2/control/RequestController.php
+++ Trabalho2/control/RequestController.php
@@ -3,15 +3,16 @@
 include_once "model/Request.php";
 class RequestController
 {
-    public function createRequest($method, $protocol, $uri, $server_addr)
+    public function createRequest($method, $protocol, $server_addr, $uri)
     {
         $uri_array = explode("/", $uri);
         return new Request(
             $method,
             $protocol,
+            $server_addr,
             $uri_array[2],
-            $this->getParams($uri_array[3]),
-            $server_addr);
+            $this->getParams($uri_array[3])
+            );
 
     }
     public function getParams($string_params)
